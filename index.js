18b00ec97365f1abf59e530063c9b85eccb10693
FrancisFlow <iamngigikaranja1@aol.com>
2021-07-17 14:20:10 +0300

    Created function akanNameGenerator, assigned Akan names to arrays and wrote conditional statements for the operation

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function akanNameGenerator(day, month, year, gender) {
+  let arrayMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let arrayFemale = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+  if(day<=0 || day>31 || month<=0 || month>12){
+    return alert("Invalid date, please enter a valid date");
+  }
+else if (gender==female) {
+  return document.getElementById("answer").innerHTML = "Your Akan name is " + arrayFemale[d];
 
+}
+else if (gender==male) {
+  return document.getElementById("answer").innerHTML = "Your Akan name is " + arrayMale[d];
+}
+else(console.log(day);)
+
+}
