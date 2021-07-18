cb08a7db3104fd583043a7be0da1d5a0931a3cef
FrancisFlow <iamngigikaranja1@aol.com>
2021-07-18 12:44:03 +0300

    Added a patch to correct for dayNumber being 7

--- index.js
+++ index.js
@@ -10,17 +10,25 @@ day=document.querySelector('#day').value;
   mm = parseInt(month);
   dd = parseInt(day);
 
-  d = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+  approximateDayNumber= (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+  exactDate=Math.round(approximateDayNumber);
+  //Patch to resolve day being 7
+  dayNumber = exactDate;
+  if (dayNumber==7) {
+    dayNumber = dayNumber-dayNumber;
+  }
+ else{ dayNumber=dayNumber;}
+  let dateArray=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let arrayMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   let arrayFemale = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
   if (dd <= 0 || dd > 31 || mm <= 0 || mm > 12) {
     return alert("Invalid date, please enter a valid date");
   } else if (gender == "Female") {
-    return document.getElementById("answer").innerHTML = "Your Akan name is" + " " + arrayFemale[Math.round(d)];
+    return document.getElementById("answer").innerHTML = "You were born on a"+ " " + dateArray[dayNumber] + ". Your Akan name is" + " " + arrayFemale[dayNumber];
 
   } else if (gender == "Male") {
-    return document.getElementById("answer").innerHTML = "Your Akan name is " + " " + arrayMale[Math.round(d)];
-  } else(console.log(day))
+    return document.getElementById("answer").innerHTML = "You were born on a"+ " " + dateArray[dayNumber] +". Your Akan name is " + " " + arrayMale[dayNumber];
+  } else{console.log(day);}
 
 }
 function removeAnswer() {
